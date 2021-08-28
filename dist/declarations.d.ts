3d1ea699b309e2e06fb8e0ac8fdc9b4b3bcf41d3
hoang <vanhoang22021997@gmail.com>
2021-08-28 15:31:00 +0700

    first commit

---
+++ dist/declarations.d.ts
@@ -0,0 +1,10 @@
+interface Kardiachain {
+    send: unknown;
+    enable: () => Promise<string[]>;
+    on?: (method: string, listener: (...args: any[]) => void) => void;
+    removeListener?: (method: string, listener: (...args: any[]) => void) => void;
+}
+declare interface Window {
+    kardiachain?: Kardiachain;
+}
+declare const __DEV__: boolean;
